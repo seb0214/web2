d52cc6a6c357766c8042948e22c2965aa1986062
seb0214 <sebastien.dewilde@student.vinci.be>
2023-09-24 17:32:12 +0200

    problem commit 1.1

--- exercises/1.1/app.js
+++ exercises/1.1/app.js
@@ -4,29 +4,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/myMovies');
-var pizzaRouter = require('./routes/pizzas');
 
 var app = express();
-
-const requete = {};
-
-app.use((req, res, next) => {
-    const path = req.path;
-
-    if (!requete[path]) {
-        requete[path] = 1;
-    } else {
-        requete[path]++;
-    }
-
-    console.log('Request counter :')
-
-    for (const path in requete) {
-        console.log('- GET ' + path + ' : ' + requete[path]);
-    }
-
-    next();
-});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,6 +13,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/pizzas', pizzaRouter);
 
 module.exports = app;

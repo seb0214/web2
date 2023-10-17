e76ccf475266310da82bcd6dd9b0cd29fe3307b8
seb0214 <sebastien.dewilde@student.vinci.be>
2023-10-17 12:36:30 +0200

    2.3.2 : event object & multiple divs

---
+++ exercises/2.3.2/index.js
@@ -0,0 +1,9 @@
+const divs = document.querySelectorAll('div');
+
+divs.forEach((div) => {
+    div.addEventListener('click', (e) => {
+        e.target.innerText = e.target.style.backgroundColor;
+        e.target.style.width = "100px";
+        e.target.style.height = "100px";
+    });
+});

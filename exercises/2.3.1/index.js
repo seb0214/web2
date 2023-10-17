bba8793401db114552168cbfe30584b072797760
seb0214 <sebastien.dewilde@student.vinci.be>
2023-10-17 10:59:50 +0200

    1.9 : API : fat model

---
+++ exercises/2.3.1/index.js
@@ -0,0 +1,7 @@
+let souhait = document.querySelector(".souhait");
+let formulaire = document.querySelector("form");
+let valeurSouhait = document.querySelector(".valeurSouhait");
+
+formulaire.addEventListener("submit", (e) => {
+    
+})
